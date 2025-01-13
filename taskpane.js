02a1211323d734f0f933a89b8341e5cbcef71dec
ousinousin <ousinousin@gmail.com>
2025-01-13 20:00:08 +0900

    Add files via upload

--- taskpane.js
+++ taskpane.js
@@ -1,13 +1,18 @@
 Office.onReady((info) => {
-    if (info.host === Office.HostType.Excel) {
-        console.log("Excel is ready");
+    // アドインが読み込まれたときに呼び出される
+    if (info.host === Office.HostType.Word) {
+        document.getElementById("run").onclick = () => tryInsertText();
     }
 });
 
-document.getElementById("run").addEventListener("click", () => {
-    Excel.run(async (context) => {
-        const sheet = context.workbook.worksheets.getActiveWorksheet();
-        sheet.getRange("A1").values = [["Hello, Office Add-ins!"]];
-        await context.sync();
-    });
-});
+async function tryInsertText() {
+    try {
+        await Word.run(async (context) => {
+            const docBody = context.document.body;
+            docBody.insertText("Hello, Word Add-in!", Word.InsertLocation.end);
+            await context.sync();
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
